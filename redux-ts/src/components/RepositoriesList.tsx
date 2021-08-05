01c0b70836689dc40c287c7eca5a643e07d3f2c1
Julien Orjollet <oj.professionnel@gmail.com>
2021-08-05 13:22:09 +0200

    started to select data fetched state

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { useActions } from '../hooks/useActions'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const { searchRepositories } = useActions()
+  const { data, loading, error } = useSelector(
+    (state: any) => state.repositories)
+  // console.log(state )
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
